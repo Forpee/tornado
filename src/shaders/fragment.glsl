45e744e966a32f17867bfb4dcee09a7ce761ce0b
Forpee <khalilhassam3@gmail.com>
2022-05-20 19:45:52 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,5 +4,10 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor=vec4(vUv+mod(uTime,.5),1.,1.);
+    // white color
+    vec3 color=vec3(1.);
+    if(vUv.x>mod(uTime,2.)||vUv.x+1.<mod(uTime,2.)){
+        discard;
+    }
+    gl_FragColor=vec4(color,1.);
 }
